8069d9c86676eaf9da1f76e36df361fdc7f2caf9
Remco Brilstra <r.j.brilstra@hotmail.com>
2013-09-25 12:03:32 +0200

    Initial commit

---
+++ source/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using VNMC2013.Resources;
+
+namespace VNMC2013
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
