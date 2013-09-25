c7d314248d1f952d61e3398fae056022e902c702
Remco Brilstra <r.j.brilstra@hotmail.com>
2013-09-25 13:54:41 +0200

    Added some structure

---
+++ source/Root.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace VNMC2013
+{
+    public partial class Root : PhoneApplicationPage
+    {
+        public Root()
+        {
+            InitializeComponent();
+        }
+
+        private void Programma_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Programma.xaml",UriKind.Relative));
+        }
+        private void Map_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show("hoi");
+        }
+        private void Poi_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show("hoi");
+        }
+        private void Rectangle_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show("hoi");
+        }
+
+    }
+}
